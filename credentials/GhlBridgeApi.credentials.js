1d8930fa36e35f9d0a14d6596d274a3e1a5e0884
Somrat <58769763+tfsomrat@users.noreply.github.com>
2026-04-30 12:57:18 +0600

    refactor: reorder credential properties to place Location ID before Bridge Key

--- credentials/GhlBridgeApi.credentials.js
+++ credentials/GhlBridgeApi.credentials.js
@@ -2,37 +2,39 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GhlBridgeApi = void 0;
 class GhlBridgeApi {
-    constructor() {
-        this.name = 'ghlBridgeApi';
-        this.displayName = 'GHL Bridge API';
-        this.documentationUrl = '';
-        this.properties = [
-            {
-                displayName: 'Base URL',
-                name: 'baseUrl',
-                type: 'string',
-                default: 'https://hl-n8n.zeon.studio',
-                required: true,
-                description: 'The URL of your Token Broker backend (use http://localhost:3000 for local testing)',
-            },
-            {
-                displayName: 'Bridge Key',
-                name: 'bridgeKey',
-                type: 'string',
-                typeOptions: { password: true },
-                default: '',
-                required: true,
-                description: 'The GHL Bridge Key obtained from the dashboard (brk_...)',
-            },
-            {
-                displayName: 'Location ID',
-                name: 'locationId',
-                type: 'string',
-                default: '',
-                required: true,
-                description: 'The GoHighLevel Location ID associated with this bridge key',
-            },
-        ];
-    }
+  constructor() {
+    this.name = "ghlBridgeApi";
+    this.displayName = "GHL Bridge API";
+    this.documentationUrl = "";
+    this.properties = [
+      {
+        displayName: "Base URL",
+        name: "baseUrl",
+        type: "string",
+        default: "https://hl-n8n.zeon.studio",
+        required: true,
+        description:
+          "The URL of your Token Broker backend (use http://localhost:3000 for local testing)",
+      },
+      {
+        displayName: "Location ID",
+        name: "locationId",
+        type: "string",
+        default: "",
+        required: true,
+        description:
+          "The GoHighLevel Location ID associated with this bridge key",
+      },
+      {
+        displayName: "Bridge Key",
+        name: "bridgeKey",
+        type: "string",
+        typeOptions: { password: true },
+        default: "",
+        required: true,
+        description: "The GHL Bridge Key obtained from the dashboard (brk_...)",
+      },
+    ];
+  }
 }
 exports.GhlBridgeApi = GhlBridgeApi;
--- credentials/GhlBridgeApi.credentials.ts
+++ credentials/GhlBridgeApi.credentials.ts
@@ -1,38 +1,37 @@
-import {
-  ICredentialType,
-  INodeProperties,
-} from 'n8n-workflow';
+import { ICredentialType, INodeProperties } from "n8n-workflow";
 
 export class GhlBridgeApi implements ICredentialType {
-  name = 'ghlBridgeApi';
-  displayName = 'GHL Bridge API';
-  documentationUrl = '';
-  
+  name = "ghlBridgeApi";
+  displayName = "GHL Bridge API";
+  documentationUrl = "";
+
   properties: INodeProperties[] = [
     {
-      displayName: 'Base URL',
-      name: 'baseUrl',
-      type: 'string',
-      default: 'https://hl-n8n.zeon.studio',
+      displayName: "Base URL",
+      name: "baseUrl",
+      type: "string",
+      default: "https://hl-n8n.zeon.studio",
       required: true,
-      description: 'The URL of your Token Broker backend (use http://localhost:3000 for local testing)',
+      description:
+        "The URL of your Token Broker backend (use http://localhost:3000 for local testing)",
     },
     {
-      displayName: 'Bridge Key',
-      name: 'bridgeKey',
-      type: 'string',
+      displayName: "Location ID",
+      name: "locationId",
+      type: "string",
+      default: "",
+      required: true,
+      description:
+        "The GoHighLevel Location ID associated with this bridge key",
+    },
+    {
+      displayName: "Bridge Key",
+      name: "bridgeKey",
+      type: "string",
       typeOptions: { password: true },
-      default: '',
+      default: "",
       required: true,
-      description: 'The GHL Bridge Key obtained from the dashboard (brk_...)',
-    },
-    {
-      displayName: 'Location ID',
-      name: 'locationId',
-      type: 'string',
-      default: '',
-      required: true,
-      description: 'The GoHighLevel Location ID associated with this bridge key',
+      description: "The GHL Bridge Key obtained from the dashboard (brk_...)",
     },
   ];
 }
